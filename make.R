ff43f2a5cf4267cdb0c7855c4fa4c2f3af38d42f
Mike Cuoco <mcuoco12@gmail.com>
2020-06-11 13:38:05 -0400

    improved dep graph

--- make.R
+++ make.R
@@ -4,4 +4,4 @@ source("R/functions.R")
 source("R/plan.R") 
 
 make(plan)
-vis_drake_graph(plan, file = "dependency_graph.png")
+vis_drake_graph(plan, file = "dependency_graph.png", targets_only = T, navigationButtons = F)
