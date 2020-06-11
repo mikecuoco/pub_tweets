5f222b7d04f5a22cce5c146db67c067298bb8412
Mike Cuoco <mcuoco12@gmail.com>
2020-06-11 19:42:24 -0400

    formatting and opts_knit

--- make.R
+++ make.R
@@ -6,10 +6,15 @@
 #' always_allow_html: true
 #' ---
 
+#+ setup
+knitr::opts_knit$set(progress = TRUE, verbose = TRUE, echo = TRUE)
 source("R/functions.R") 
 source("R/plan.R") 
 
+#+ make from scratch
 clean()
 make(plan)
+
+#+ visualize build
 vis_drake_graph(plan, targets_only = T, navigationButtons = F)
 build_times() %>% gt()
